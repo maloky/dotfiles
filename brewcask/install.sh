f0e087c813d29caa74d12a3aa7ebac24000b99fd
Jorge Costa <joky@maloky.com>
2015-11-09 16:39:13 +0000

    slack instead of hipchat

--- brewcask/install.sh
+++ brewcask/install.sh
@@ -3,7 +3,7 @@ brew install caskroom/cask/brew-cask
 brew cask install dropbox
 brew cask install evernote
 brew cask install google-chrome
-brew cask install hipchat
+brew cask install slack 
 brew cask install iterm2
 brew cask install sequel-pro
 brew cask install spotify
@@ -11,4 +11,3 @@ brew cask install the-unarchiver
 brew cask install transmission
 brew cask install vlc
 brew cask install transmit
-brew cask install transmission
